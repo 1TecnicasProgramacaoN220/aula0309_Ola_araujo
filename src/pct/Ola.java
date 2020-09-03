26633bb3f52217aadf21bc14f1d8653ef67bf8c4
Temporário <Temporrio@DESKTOP-HIO5648>
2020-09-03 20:18:24 -0300

    Primeiro commit java

---
+++ src/pct/Ola.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Temporário
+ */
+public class Ola {
+    
+    public static void main(String[] args) {
+        //atalho - digite psvm e clique tab para criar
+        
+        System.out.println("Olá");
+        //atalho - digite sout e clique tab para criar
+    
+        JOptionPane.showMessageDialog (null, "Olá");
+        
+        
+        
+    }
+    
+    
+    
+}
